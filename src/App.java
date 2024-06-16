2c57909196c8ce768de8e1950740dca8bc551120
muriisantuss <muriisantuss@gmail.com>
2024-06-16 17:29:22 -0300

    set attributes and initialize with constuctor

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        int[] value = { 10, 10, 10 };
+        linearList l = new linearList(value, 10);
 
+        System.out.println(l.list[1] + l.nroElem);
     }
 }
--- src/linearList.java
+++ src/linearList.java
@@ -1,3 +1,13 @@
 public class linearList {
- 
+  public int[] list = new int[10];
+  public int nroElem;
+
+  public linearList() {
+  }
+
+  public linearList(int[] list, int nroElem) {
+    this.list = list;
+    this.nroElem = nroElem;
+  }
+
 }
